de97aa6c4f2199da34fb5e6a39acb660a8556751
jspc <james+git@zero-internet.org.uk>
2023-07-23 16:10:44 +0100

    Look user keys up against the key server

---
+++ auth.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"crypto/tls"
+
+	"github.com/rotter-git/rotter-keys/server"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+)
+
+type KeysClient struct {
+	ksc server.KeyServiceClient
+}
+
+func NewKeyServiceClient(addr string) (c *KeysClient, err error) {
+	conf := grpc.WithTransportCredentials(credentials.NewTLS(&tls.Config{
+		MinVersion:         tls.VersionTLS12,
+		InsecureSkipVerify: true,
+	}))
+
+	conn, err := grpc.Dial(addr, conf)
+	if err != nil {
+		return
+	}
+
+	c = new(KeysClient)
+	c.ksc = server.NewKeyServiceClient(conn)
+
+	return
+}
+
+func (c *KeysClient) Lookup(key []byte) (u *server.User, err error) {
+	return c.ksc.Lookup(context.Background(), &server.PublicKey{
+		Contents: key,
+	})
+}
